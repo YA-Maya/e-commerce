601bac8bb0e5739d058997ad05020ea90941b1c7
Ya-Maya <Sumeyask6@gmail.com>
2023-08-22 18:44:08 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>
+  },
+  {
+    path: "/products/:id",
+    element: <span>Category</span>,
+  },
+  {
+    path: "/product/:id",
+    element: <span>Product</span>
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router}>
+      </RouterProvider>
+    </div>
+  )
+}
+
+export default App;
